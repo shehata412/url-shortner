0709b68f81d23b9b3b340f986d35cc7bf4e0eedd
shehata412 <shehata412@aucegypt.edu>
2024-03-05 13:12:16 +0200

    add auth

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -11,3 +11,43 @@ interface UserRequest extends Request {
     username: string;
   };
 }
+
+type User_data = { 
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+};
+
+
+export const authenticate = async (
+  req: UserRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    return res.status(401).send({ error: "Please authenticate" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+    const user = await User.findOne({
+      where: {
+        id: decoded.id,
+      },
+    });
+
+    const userData: User_data = user?.get({ plain: true }) as User_data;
+
+    if (!user) {
+      throw new Error();
+    }
+    req.user = {
+      id: userData.id,
+      username: userData.username,
+    };
+    next();
+  } catch (e) {
+    res.status(401).send({ error: "Please authenticate" });
+  }
+};
